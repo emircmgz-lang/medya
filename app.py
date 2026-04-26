87a661b6b730f879ee79d0062bb1ce1ffa0f380b
emircmgz-lang <emir_cmgz@outlook.com>
2026-04-26 13:51:14 +0000

    final gemini http

--- app.py
+++ app.py
@@ -28,8 +28,7 @@ def ai_analiz(text):
     try:
         API_KEY = os.environ.get("GEMINI_API_KEY")
 
-        url = f"https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key={API_KEY}"
-
+        url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={API_KEY}"
         payload = {
             "contents": [
                 {
